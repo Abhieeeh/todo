43b6658213d671ac51f7fc23451c879dfc14fac4
Abhieeeh <abhishekkvilakkode@gmail.com>
2026-04-05 16:11:04 +0530

    delete todo added

--- client/src/components/todos.jsx
+++ client/src/components/todos.jsx
@@ -1,20 +1,12 @@
-import react, { useState ,useEffect} from 'react';
+import react, { useState ,useEffect,useCallback} from 'react';
 
 function Todos() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
   
-
-
-
-
-
-
-
-
   
-  useEffect(() => {
-    const fetchTodos=async()=>{
+
+  const fetchTodos=async()=>{
     const token = localStorage.getItem('token');
     const response = await fetch ("/todos",{
       method: 'GET',
@@ -27,9 +19,29 @@ function Todos() {
     console.log(data.message);
     setTodos(data.todos);
   } 
+
+
+  useEffect(() => { 
   fetchTodos();
   }, []);
 
+ 
+ 
+
+const completedTodo=async()=>{
+  const token = localStorage.getItem('token');
+  const response = await fetch ("/todos/completed",{
+    method: 'GET',
+    headers:{
+      "content-Type":"application/json",
+      "Token":token
+    },  
+    //body:JSON.stringify({is_completed:true})
+  });
+  const data = await response.json();
+  console.log(data.message);
+  setTodos(data.todos); 
+  };
 
 
 
@@ -39,6 +51,47 @@ function Todos() {
 
 
 
+
+
+
+const DeleteHandle=async(item)=>{
+  const token = localStorage.getItem('token');
+  const response=await fetch("/todos",{
+    method:"DELETE",
+    headers:{
+      "content-Type":"application/json",
+      "token":token,
+      
+    },
+    body:JSON.stringify({item:item})
+  });
+  console.log(response);
+  fetchTodos();
+
+
+}
+
+
+
+const handleCompleted=async(item)=>{
+  const token = localStorage.getItem('token');
+  const response=await fetch("/todos",{
+    method:"PUT",
+    headers:{
+      "content-Type":"application/json",
+      "token":token,
+      "item":item
+    }
+  });
+  console.log(response);
+  fetchTodos();
+}
+
+
+
+
+
+  
 
 
   const handleclick=async(e)=>{
@@ -62,13 +115,16 @@ function Todos() {
 return (
   <div>
     <h1>Todo List</h1>
-    <input type="text" value={newTodo} onChange={(e)=>setNewTodo(e.target.value)} placeholder="Enter your todo"></input>
+    <input type="text" value={newTodo} onChange={(e)=>setNewTodo(e.target.value)} placeholder="Enter your todo" required/>
     <button onClick={handleclick}>Add</button>
+    <button onClick={completedTodo}>Completed Tasks</button>
     <ul>
-      {todos.map((item,i)=><li key={item}>{item} <button>Delete</button></li>)}
+      {todos.map((item,i)=><li key={item}>{item} <button onClick={()=>DeleteHandle(item)}>Delete</button> <button onClick={()=>handleCompleted(item)}>COMPLETED</button></li>)}
     </ul>
     
   </div>
 );
-}
+} 
+
+
 export default Todos;
